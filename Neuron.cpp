32e9df636933ebfec4c2eac633c935743797536b
Aleksey Filippov <sarum9in@gmail.com>
2013-06-18 18:52:28 +0400

    Neuron: activation function and weights access implementation.

--- Neuron.cpp
+++ Neuron.cpp
@@ -1,7 +1,54 @@
 #include "Neuron.hpp"
 
-Neuron::Neuron()
+#include <QDebug>
+
+Neuron::Neuron(): m_weights(1) {}
+
+Neuron::Neuron(const ActivationFunctionPointer &activationFunction):
+    m_activationFunction(activationFunction),
+    m_weights(1) {}
+
+Neuron::Neuron(const ActivationFunctionPointer &activationFunction, const WeightVector &weights):
+    m_activationFunction(activationFunction),
+    m_weights(weights)
 {
+    Q_ASSERT(!weights.empty());
+}
+
+bool Neuron::hasActivationFunction() const
+{
+    return !m_activationFunction.isNull();
+}
+
+const ActivationFunctionPointer &Neuron::activationFunction() const
+{
+    return m_activationFunction;
+}
+
+void Neuron::setActivationFunction(const ActivationFunctionPointer &activationFunction)
+{
+    m_activationFunction = activationFunction;
+}
+
+WeightVector &Neuron::weights()
+{
+    return m_weights;
+}
+
+const WeightVector &Neuron::weights() const
+{
+    return m_weights;
+}
+
+void Neuron::setWeights(const WeightVector &weights)
+{
+    Q_ASSERT(!weights.empty());
+    m_weights = weights;
+}
+
+int Neuron::inputNumber() const
+{
+    return m_weights.size() - 1;
 }
 
 int Neuron::outputNumber() const
--- Neuron.hpp
+++ Neuron.hpp
@@ -3,13 +3,36 @@
 
 #include "neuro_global.hpp"
 
+#include "ActivationFunction.hpp"
+
+#include <QVector>
+
+typedef QVector<qreal> WeightVector;
+
 class NEUROSHARED_EXPORT Neuron
 {
 public:
     Neuron();
+    explicit Neuron(const ActivationFunctionPointer &activationFunction);
+    Neuron(const ActivationFunctionPointer &activationFunction, const WeightVector &weights);
+
+    /// Check if ActivationFunction is initialized.
+    bool hasActivationFunction() const;
+    const ActivationFunctionPointer &activationFunction() const;
+    void setActivationFunction(const ActivationFunctionPointer &activationFunction);
+
+    /// \note: 0th element is bias
+    WeightVector &weights();
+    /// \copydoc weights()
+    const WeightVector &weights() const;
+    void setWeights(const WeightVector &weights);
 
     int inputNumber() const;
     int outputNumber() const;
+
+private:
+    ActivationFunctionPointer m_activationFunction;
+    WeightVector m_weights;
 };
 
 #endif // NEURON_HPP
